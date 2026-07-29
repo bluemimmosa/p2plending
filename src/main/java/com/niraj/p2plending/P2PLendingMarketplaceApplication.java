3e01faec40db348e53fe1ba6915a9c0ec9739185
bluemimmosa <console1913@gmail.com>
2026-07-29 19:56:03 +0545

    Initial Project Structure

---
+++ src/main/java/com/niraj/p2plending/P2PLendingMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.niraj.p2plending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2PLendingMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P2PLendingMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niraj/p2plending/P2PLendingMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niraj.p2plending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2PLendingMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
